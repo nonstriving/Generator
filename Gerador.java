2c365bbb792cf2d0e6b1de53aeb762655d74c409
samara <samara@Samaras-MacBook-Pro.local>
2018-10-09 23:28:03 -0300

    bla

--- Gerador.java
+++ Gerador.java
@@ -3,3 +3,16 @@ import java.util.ArrayList;
 
 public class Gerador extends LABaseVisitor<String>{
 	
+	private SaidaParser sp;
+	private PilhaDeTabelas pilhaTabela;
+	private String tipoMaisVar;	
+
+	public String getTipo(String tipo){
+		if(tipo.equals("inteiro"))
+			return "%d";
+		else if(tipo.equals("real"))
+			return "%f";
+		else if(tipo.equals("literal") || tipo.equals("char"))
+			return "%s";
+		return "";
+	}
