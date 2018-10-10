0aed3f876440a9c07df3018f35622428b767c272
samara <samara@Samaras-MacBook-Pro.local>
2018-10-10 11:44:18 -0300

    bla

--- Gerador.java
+++ Gerador.java
@@ -7,6 +7,9 @@ public class Gerador extends LABaseVisitor<String>{
 	private PilhaDeTabelas pilhaTabela;
 	private String tipoMaisVar;	
 
+	//public GeradorCodigo(SaidaParser sp){this.sp = sp;}
+	//?
+
 	public String getTipo(String tipo){
 		if(tipo.equals("inteiro"))
 			return "%d";
@@ -15,4 +18,20 @@ public class Gerador extends LABaseVisitor<String>{
 		else if(tipo.equals("literal") || tipo.equals("char"))
 			return "%s";
 		return "";
+	}
+
+	@Override
+	public String visitPrograma(LAParser.ProgramaContext ctx){
+		if(ctx.children != null){
+			pilhaTabela = new PilhaDeTabelas();
+			pilhaTabela.empilhar(new TabelaDeSimbolos("global"));
+			sp.println("#include <stdio.h>\n#include <stdlib.h>\n");
+			sp.println(visitDeclaracoes(ctx.declaracoes()));
+
+			sp.println("int main(){");
+			sp.prinln(visitCorpo(ctx.corpo()));
+
+			sp.println("\treturn 0;\n});
+			pilhaTabela.desempilhar();
+		}
 	}
